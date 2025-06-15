9472267acaaf8312b2b626ee8edb64707da60b61
SachindaBandara <sachindaudeni192@gmail.com>
2025-06-15 21:43:37 +0530

    fetch the data from json-server

--- vue-crash/src/components/JobsListing.vue
+++ vue-crash/src/components/JobsListing.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { RouterLink } from "vue-router";
-import { ref, defineProps } from "vue";
-import jobData from "@/jobs.json";
+import { reactive, defineProps, onMounted } from "vue";
+import axios from "axios";
+import PulseLoader from "vue-spinner/src/PulseLoader.vue";
+
 import JobListing from "./JobListing.vue";
 
 const props = defineProps({
@@ -12,7 +14,21 @@ const props = defineProps({
   },
 });
 
-const jobs = ref(jobData);
+const state = reactive({
+  jobs: [],
+  isLoading: true,
+});
+
+onMounted(async () => {
+  try {
+    const response = await axios.get("http://localhost:5000/jobs");
+    state.jobs = response.data;
+  } catch (error) {
+    console.error("Error Fetching Data!", error);
+  } finally {
+    state.isLoading = false;
+  }
+});
 </script>
 
 <template>
@@ -21,9 +37,15 @@ const jobs = ref(jobData);
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
         Browse Jobs
       </h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <!-- Show loading spinner while loading is true -->
+      <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+        <PulseLoader />
+      </div>
+
+      <!-- Show job listing when done loading -->
+      <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
         <JobListing
-          v-for="job in jobs.slice(0, limit || jobs.length)"
+          v-for="job in state.jobs.slice(0, limit || state.jobs.length)"
           :key="job.id"
           :job="job"
         />
--- vue-crash/tailwind.config.js
+++ vue-crash/tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Poppins", "sans-serif"],
+        sans: ["Inter", "sans-serif"],
       },
       gridTemplateColumns: {
         "70/30": "70% 28%",
