74af99de56c1100b6043de319fd7cb8906dbfbae
SachindaBandara <sachindaudeni192@gmail.com>
2025-06-15 19:23:41 +0530

    created Notfound page, adn jobView page

--- vue-crash/src/App.vue
+++ vue-crash/src/App.vue
@@ -1,12 +1,9 @@
 <script setup>
-import Hero from "./components/Hero.vue";
-import HomeCard from "./components/HomeCard.vue";
-import JobsListing from "./components/JobsListing.vue";
 import Navbar from "./components/Navbar.vue";
+import { RouterView } from "vue-router";
+
 </script>
 <template>
   <Navbar />
-  <Hero title="Become a Vue Job" />
-  <HomeCard />
-  <JobsListing :limit="3" :showButton="true"/>
+  <RouterView/>
 </template>
---
+++ vue-crash/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Hero from "@/components/Hero.vue";
+import HomeCard from "@/components/HomeCard.vue";
+import JobsListing from "@/components/JobsListing.vue";
+</script>
+<template>
+  <Hero title="Become a Vue Job" />
+  <HomeCard />
+  <JobsListing :limit="3" :showButton="true" />
+</template>
