faf143f9702b56967ff7fbad62718b906ac2e9be
SachindaBandara <sachindaudeni192@gmail.com>
2025-06-15 22:55:16 +0530

    created delete functionality

--- vue-crash/src/views/JobView.vue
+++ vue-crash/src/views/JobView.vue
@@ -2,16 +2,33 @@
 import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 import axios from "axios";
 import { reactive, onMounted } from "vue";
-import { useRoute, RouterLink } from "vue-router";
+import { useRoute, RouterLink, useRouter } from "vue-router";
 import BackButton from "@/components/backButton.vue";
+import { useToast } from "vue-toastification";
 
 const route = useRoute();
+const router = useRouter();
+const toast = useToast();
 
 const jobId = route.params.id;
 const state = reactive({
   job: {},
   isLoading: true,
 });
+
+const deleteJob = async () => {
+  try {
+    const confirm = window.confirm("Are you sure you want to delete this job?");
+    if (confirm) {
+      await axios.delete(`/api/jobs/${jobId}`);
+      toast.success("Job Deleted Successfully!");
+      router.push("/jobs");
+    }
+  } catch (error) {
+    console.error("Error deleting job", error);
+    toast.error("Job not deleted!");
+  }
+};
 
 onMounted(async () => {
   try {
@@ -26,7 +43,7 @@ onMounted(async () => {
 </script>
 
 <template>
-  <BackButton/>
+  <BackButton />
   <section v-if="!state.isLoading" class="bg-green-50">
     <div class="container m-auto py-10 px-6">
       <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
@@ -34,15 +51,13 @@ onMounted(async () => {
           <div
             class="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
           >
-            <div class="text-gray-500 mb-4">{{state.job.type}}</div>
-            <h1 class="text-3xl font-bold mb-4">{{state.job.title}}</h1>
+            <div class="text-gray-500 mb-4">{{ state.job.type }}</div>
+            <h1 class="text-3xl font-bold mb-4">{{ state.job.title }}</h1>
             <div
               class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
             >
-              <i
-                class="pi pi-map-marker text-xl text-orange-700 mr-2"
-              ></i>
-              <p class="text-orange-700">{{state.job.location}}</p>
+              <i class="pi pi-map-marker text-xl text-orange-700 mr-2"></i>
+              <p class="text-orange-700">{{ state.job.location }}</p>
             </div>
           </div>
 
@@ -52,12 +67,12 @@ onMounted(async () => {
             </h3>
 
             <p class="mb-4">
-              {{state.job.description}}
+              {{ state.job.description }}
             </p>
 
             <h3 class="text-green-800 text-lg font-bold mb-2">Salary</h3>
 
-            <p class="mb-4">{{state.job.salary}} / Year</p>
+            <p class="mb-4">{{ state.job.salary }} / Year</p>
           </div>
         </main>
 
@@ -67,10 +82,10 @@ onMounted(async () => {
           <div class="bg-white p-6 rounded-lg shadow-md">
             <h3 class="text-xl font-bold mb-6">Company Info</h3>
 
-            <h2 class="text-2xl">{{state.job.company.name}}</h2>
+            <h2 class="text-2xl">{{ state.job.company.name }}</h2>
 
             <p class="my-2">
-              {{state.job.company.description}}
+              {{ state.job.company.description }}
             </p>
 
             <hr class="my-4" />
@@ -78,12 +93,14 @@ onMounted(async () => {
             <h3 class="text-xl">Contact Email:</h3>
 
             <p class="my-2 bg-green-100 p-2 font-bold">
-              {{state.job.company.contactEmail}}
+              {{ state.job.company.contactEmail }}
             </p>
 
             <h3 class="text-xl">Contact Phone:</h3>
 
-            <p class="my-2 bg-green-100 p-2 font-bold">{{state.job.company.contactPhone}}</p>
+            <p class="my-2 bg-green-100 p-2 font-bold">
+              {{ state.job.company.contactPhone }}
+            </p>
           </div>
 
           <!-- Manage -->
@@ -95,6 +112,7 @@ onMounted(async () => {
               >Edit Job</RouterLink
             >
             <button
+              @click="deleteJob"
               class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
             >
               Delete Job
