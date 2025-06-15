20436413eba31beecac1df9a4e82ddfc3ca74ccb
SachindaBandara <sachindaudeni192@gmail.com>
2025-06-15 13:08:40 +0530

    created Hero section and job listing section

--- vue-crash/src/App.vue
+++ vue-crash/src/App.vue
@@ -1,6 +1,12 @@
 <script setup>
+import Hero from "./components/Hero.vue";
+import HomeCard from "./components/HomeCard.vue";
+import JobsListing from "./components/JobsListing.vue";
 import Navbar from "./components/Navbar.vue";
 </script>
 <template>
   <Navbar />
+  <Hero title="Become a Vue Job" />
+  <HomeCard />
+  <JobsListing :limit="3" :showButton="true"/>
 </template>
